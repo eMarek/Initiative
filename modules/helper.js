fad82f67fe0b690584b0515b397ec93afde8fd6b
Marko Bregant <marko@bregant.si>
2013-12-19 11:33:27 +0100

    Time stamp leading zero fix

--- modules/helper.js
+++ modules/helper.js
@@ -11,11 +11,11 @@ exports.time_stamp = function () {
 	var d = new Date();
 
 	var year = d.getFullYear();
-	var month = d.getMonth() + 1;
-	var date = d.getDate();
-	var hours = d.getHours();
-	var minutes = d.getMinutes();
-	var seconds = d.getSeconds();
+	var month = (d.getMonth() + 1) < 10 ? '0' + (d.getMonth() + 1) : (d.getMonth() + 1);
+	var date = d.getDate() < 10 ? '0' + d.getDate() : d.getDate();
+	var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
+	var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+	var seconds = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
 
 	var time_stamp = [ year, month, date ].join('-') + ' ' + [ hours, minutes, seconds ].join(':');
 
