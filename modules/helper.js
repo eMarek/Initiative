6562a7148b9452a35677717f36eff33d4b4cfac4
Marko Bregant <marko@bregant.si>
2013-12-20 21:42:57 +0100

    Simplified time stamp

--- modules/helper.js
+++ modules/helper.js
@@ -8,16 +8,8 @@ exports.install = function (framework, name) {
  
 exports.time_stamp = function () {
 
-	var d = new Date();
-
-	var year = d.getFullYear();
-	var month = (d.getMonth() + 1) < 10 ? '0' + (d.getMonth() + 1) : (d.getMonth() + 1);
-	var date = d.getDate() < 10 ? '0' + d.getDate() : d.getDate();
-	var hours = d.getHours() < 10 ? '0' + d.getHours() : d.getHours();
-	var minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
-	var seconds = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
-
-	var time_stamp = [ year, month, date ].join('-') + ' ' + [ hours, minutes, seconds ].join(':');
+	var date = new Date();
+	var time_stamp = date.format('yyyy-MM-dd HH:mm:ss');
 
 	return time_stamp;
 }
